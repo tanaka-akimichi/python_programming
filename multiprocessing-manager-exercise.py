73659107d5159f4fcdf3a7a17b3cffb272aade85
tanaka-akimichi <akimichi@sepia.ocn.ne.jp>
2018-07-17 23:28:33 +0900

    multiprocessing

---
+++ multiprocessing-manager-exercise.py
@@ -0,0 +1,28 @@
+from multiprocessing import (
+    Process,
+    Lock, RLock, Semaphore, Queue, Event, Condition, Barrier,
+    Value, Array, Pipe, Manager)
+
+import logging
+import multiprocessing
+import time
+
+logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
+
+def worker1(i):
+    logging.debug('start')
+    time.sleep(5)
+    logging.debug('end')
+    return i
+
+
+if __name__ == '__main__':
+    # t1 = multiprocessing.Process(target=worker1, args=(i,))
+    with multiprocessing.Pool(3) as p:
+        logging.debug(p.apply(worker1, (200, )))
+        logging.debug('execute apply')
+        p1 = p.apply_async(worker1, (100, ))
+        p2 = p.apply_async(worker1, (100, ))
+        logging.debug('executed')
+        logging.debug(p1.get(timeout=100))
+        logging.debug(p2.get())
